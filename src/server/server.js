fe237dd5d1c72c7ea70952d8e37744ac17f2dd15
luis_parra <luis.parra.pulido@gmail.com>
2019-11-21 20:40:57 -0500

    Preparando proyecto para integración (Backend For Frontend)

--- src/server/server.js
+++ src/server/server.js
@@ -2,6 +2,8 @@ import express from 'express';
 import dotenv from 'dotenv';
 import webpack from 'webpack';
 import helmet from 'helmet';
+import passport from 'passport';
+import boom from '@hapi/boom';
 import main from './routes/main';
 
 dotenv.config();
@@ -34,6 +36,117 @@ if (ENV === 'development') {
   app.use(helmet.permittedCrossDomainPolicies());
   app.disable('x-powered-by');
 }
+
+app.post('/auth/sign-in', async (req, res, next) => {
+  passport.authenticate('basic', (error, data) => {
+    try {
+      if (error || !data) {
+        next(boom.unauthorized());
+      }
+
+      req.login(data, { session: false }, async (error) => {
+        if (error) {
+          next(error);
+        }
+
+        const { token, ...user } = data;
+
+        res.cookie('token', token, {
+          httpOnly: !config.dev,
+          secure: !config.dev,
+          // maxAge: rememberMe ? THIRTY_DAYS_IN_SEC : TWO_HOURS_IN_SEC
+        });
+
+        res.status(200).json(user);
+      });
+    } catch (error) {
+      next(error);
+    }
+  })(req, res, next);
+});
+
+app.post('/auth/sign-up', async (req, res, next) => {
+  const { body: user } = req;
+
+  try {
+    await axios({
+      url: `${config.apiUrl}/api/auth/sign-up`,
+      method: 'post',
+      data: user,
+    });
+
+    res.status(201).json({ message: 'user created'});
+  } catch (error) {
+    next(error);
+  }
+});
+
+app.get('/tickets', async (req, res, next) => {
+
+});
+
+app.post('/user-tickets', async (req, res, next) => {
+  try {
+    const { body: userTicket } = req;
+    const { token } = req.cookies;
+
+    const { data, status } = await axios({
+      url: `${config.apiUrl}/api/user-tickets`,
+      headers: { Authorization: `Bearer ${token}` },
+      method: 'post',
+      data: userTicket,
+    });
+
+    if (status !== 201) {
+      return next(boom.badImplementation());
+    }
+
+    res.status(201).json(data);
+  } catch (error) {
+    next(error);
+  }
+});
+
+app.delete('/user-tickets/:userTicketId', async (req, res, next) => {
+  try {
+    const { userTicketId } = req.params;
+    const { token } = req.cookies;
+
+    const { data, status } = await axios({
+      url: `${config.apiUrl}/api/user-tickets/${userTicketId}`,
+      headers: { Authorization: `Bearer ${token}` },
+      method: 'delete',
+    });
+
+    if (status !== 200) {
+      return next(boom.badImplementation());
+    }
+
+    res.status(200).json(data);
+  } catch (error) {
+    next(error);
+  }
+});
+
+app.get('/auth/google-oauth', passport.authenticate('google-oauth', {
+  scope: ['email', 'profile', 'openid'],
+}));
+
+app.get('/auth/google-oauth/callback', passport.authenticate('google-oauth', { session: false }),
+  (req, res, next) => {
+    if (!req.user) {
+      next(boom.unauthorized());
+    }
+
+    const { token, ...user } = req.user;
+
+    res.cookie('token', token, {
+      httpOnly: !config.dev,
+      secure: !config.dev,
+    });
+
+    res.status(200).json(user);
+  });
 
 app.get('*', main);
 
